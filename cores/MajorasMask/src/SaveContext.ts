1dde3b18fcdee55a5fbb9c8e3c4185dfd64c37cf
Denoflions <denvstheworld@gmail.com>
2020-11-12 06:01:28 -0500

    Wrong size here.

--- cores/MajorasMask/src/SaveContext.ts
+++ cores/MajorasMask/src/SaveContext.ts
@@ -460,7 +460,7 @@ export class SaveContext extends JSONTemplate implements API.ISaveContext {
     }
 
     get permFlags(): Buffer {
-        return this.emulator.rdramReadBuffer(this.offsets.permFlags, 0x960);
+        return this.emulator.rdramReadBuffer(this.offsets.permFlags, 0x710);
     }
 
     set permFlags(b: Buffer) {
